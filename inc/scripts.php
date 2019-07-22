6d5c4ed73bf929c19a3049163ac37ddac3614ebf
Joe Bailey-Roberts <dev@joebr.io>
2019-07-22 22:07:55 +0100

    Setup webp utility

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,7 +11,12 @@
  namespace jbr\scripts;
 
 function init() {
-	wp_register_style( 'jbr-styles', get_template_directory_uri() . '/dist/main.min.css', array(), filemtime( get_template_directory() . '/dist/main.min.css' ), 'all' );
+    wp_register_style( 'jbr-styles', get_template_directory_uri() . '/dist/main.min.css', array(), filemtime( get_template_directory() . '/dist/main.min.css' ), 'all' );
+    
+    wp_enqueue_style('jbr-styles');
+
+
+    wp_register_script('jbr-scripts', get_template_directory_uri() . '/dist/main.js', array('jquery'), filemtime( get_template_directory() . '/dist/main.js' ), true );
 }
 
  add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\init');
---
+++ inc/utilities/utilities.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Utilty functions
+ * 
+ * @package jbr
+ * @since 0.0.3
+ */
+
+ namespace jbr\utilities;
+
+ function picture($id, $size = 'thumbnail'){
+     $url = wp_get_attachment_image_url($id, $size);
+     ?>
+    <picture>
+    <source srcset="<?php echo $url ?>.webp" type="image/webp">
+    <source srcset="<?php echo $url ?>" type="<?php echo get_post_mime_type($id) ?>">
+    <?php echo wp_get_attachment_image($id, $size); ?>
+    </picture>
+<?php
+ }
