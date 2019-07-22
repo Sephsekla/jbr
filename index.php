2d82aa95bbaf77fe3a2674430f990e6969489439
Joe Bailey-Roberts <dev@joebr.io>
2019-07-22 21:21:11 +0100

    Create setup functions and initial build

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The main template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package jbe
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+
+
+			/* Start the Loop */
+		while ( have_posts() ) {
+			the_post();
+
+			/*
+			 * Include the Post-Type-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+			 */
+			get_template_part( 'template-parts/content', get_post_type() );
+
+		}
+
+			the_posts_navigation();
+
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
