0560d30e625b909a42ed76a97e3f82987873d55b
Joe Bailey-Roberts <dev@joebr.io>
2019-08-04 13:46:15 +0100

    Add banner

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,17 +11,19 @@
  namespace jbr\scripts;
 
 function init() {
-    wp_register_style( 'jbr-styles', get_template_directory_uri() . '/dist/main.min.css', array(), filemtime( get_template_directory() . '/dist/main.min.css' ), 'all' );
-    
-    wp_enqueue_style('jbr-styles');
 
+	wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700|Roboto:400,700&display=swap', array(), date( 'M' ), 'all' );
 
-    wp_register_script('jbr-scripts', get_template_directory_uri() . '/dist/main.js', array('jquery'), filemtime( get_template_directory() . '/dist/main.js' ), true );
+	wp_register_style( 'jbr-styles', get_template_directory_uri() . '/dist/main.min.css', array( 'google-fonts' ), filemtime( get_template_directory() . '/dist/main.min.css' ), 'all' );
 
-    wp_enqueue_script('jbr-scripts');
+	wp_enqueue_style( 'jbr-styles' );
+
+	wp_register_script( 'jbr-scripts', get_template_directory_uri() . '/dist/main.js', array( 'jquery' ), filemtime( get_template_directory() . '/dist/main.js' ), true );
+
+	wp_enqueue_script( 'jbr-scripts' );
 
 }
 
- add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\init');
+ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\init' );
 
 
