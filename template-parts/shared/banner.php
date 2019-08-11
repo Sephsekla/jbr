5ed4fcf15c535abd5ca593c47168d3d4b6270a12
Joe Bailey-Roberts <dev@joebr.io>
2019-08-11 08:59:37 +0100

    Add banner experiment

--- template-parts/shared/banner.php
+++ template-parts/shared/banner.php
@@ -1,13 +1,12 @@
-<?php 
+<?php
 
 
 $id = get_queried_object_id();
 
-if(get_post_type($id) === 'post'){
-    $id = get_option('page_for_posts');
-}
-elseif(is_archive()){
-    $id = get_option('page_for_posts');
+if ( get_post_type( $id ) === 'post' ) {
+	$id = get_option( 'page_for_posts' );
+} elseif ( is_archive() ) {
+	$id = get_option( 'page_for_posts' );
 }
 
 
@@ -19,10 +18,10 @@ $title = get_the_title( $id );
 
 
 <section class="banner" style="background-image: url(<?php echo wp_get_attachment_image_url( $image_id, 'banner' ); ?>)">
-<?php jbr\utilities\picture($image_id,'banner'); ?>
+<?php jbr\utilities\picture( $image_id, 'banner' ); ?>
 <div class="inner">
 <div class="container-fluid">
-<h1 style="background-image: url(<?php echo wp_get_attachment_image_url( $image_id, 'banner' ); ?>)"><?php echo $title; ?></h1>
+<h1 style="background-image: url(<?php echo wp_get_attachment_image_url( $image_id, 'banner' ); ?>)" data-title="<?php echo $title; ?>"><?php echo $title; ?></h1>
 </div>
 </div>
 </section> 
