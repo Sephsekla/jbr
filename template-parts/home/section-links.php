c05e54e2257d0bb5d63d54637910c3956496086e
Joe Bailey-Roberts <dev@joebr.io>
2020-02-10 15:39:30 +0000

    Add banner links

---
+++ template-parts/home/section-links.php
@@ -0,0 +1,21 @@
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-4">
+                <a class="linkbox">
+                    About Me
+                </a>
+            </div>
+            <div class="col-12 col-lg-4">
+                <a class="linkbox">
+                    Past Projects
+                </a>
+            </div>
+            <div class="col-12 col-lg-4">
+                <a class="linkbox">
+                    Let's work together
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
