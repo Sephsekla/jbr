0b7cc5abd83a491cbbd661b52c54b02a3bd0a246
Joe Bailey-Roberts <dev@joebr.io>
2020-02-11 12:46:39 +0000

    Add image hover effect

--- template-parts/home/section-links.php
+++ template-parts/home/section-links.php
@@ -12,9 +12,21 @@ $links[] = get_field('link_3');
             <?php foreach($links as $link){ ?>
 
             <div class="col-12 col-lg-4">
-                <a href="<?php echo $link['link']['url'] ?>" class="linkbox" style="background-image: url(<?php echo wp_get_attachment_image_url($link['image'],'full') ?>)">
+                <a href="<?php echo $link['link']['url'] ?>" class="linkbox">
+                <div class="image" style="background-image: url(<?php echo wp_get_attachment_image_url($link['image'],'full') ?>)">
+
                 <?php jbr\utilities\picture($link['image'],'full') ?>
+                </div>
+
+                <div class="image alt" style="background-image: url(<?php echo wp_get_attachment_image_url($link['image_hover'],'full') ?>)">
+
+<?php jbr\utilities\picture($link['image_hover'],'full') ?>
+</div>
+
+                <div class="overlay">
                     <?php echo $link['link']['title'] ?>
+                </div>   
+
                 </a>
             </div>
            
