1e40821ad20b269b5dc9a6ecb6a9aebb77c9ef42
Joe Bailey-Roberts <dev@joebr.io>
2020-02-11 11:42:19 +0000

    Setup loop of homelinks

--- template-parts/home/section-links.php
+++ template-parts/home/section-links.php
@@ -1,21 +1,28 @@
-<section>
+<?php $links = [];
+
+$links[] = get_field('link_1');
+$links[] = get_field('link_2');
+$links[] = get_field('link_3');
+
+
+?>
+<section class="link-boxes">
     <div class="container">
         <div class="row">
+            <?php foreach($links as $link){ ?>
+
             <div class="col-12 col-lg-4">
-                <a class="linkbox">
-                    About Me
+                <a href="<?php echo $link['link']['url'] ?>" class="linkbox" style="background-image: url(<?php echo wp_get_attachment_image_url($link['image'],'full') ?>)">
+                <?php jbr\utilities\picture($link['image'],'full') ?>
+                    <?php echo $link['link']['title'] ?>
                 </a>
             </div>
-            <div class="col-12 col-lg-4">
-                <a class="linkbox">
-                    Past Projects
-                </a>
-            </div>
-            <div class="col-12 col-lg-4">
-                <a class="linkbox">
-                    Let's work together
-                </a>
-            </div>
+           
+            <?php 
+
+            }
+
+            ?>
         </div>
     </div>
 </section>
