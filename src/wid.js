c99b614ac039a575665525701cae5a98a4c9ee68
Joe Bailey-Roberts <dev@joebr.io>
2020-02-17 10:04:16 +0000

    Slidedown expansion

--- src/wid.js
+++ src/wid.js
@@ -20,9 +20,9 @@ let options = {
 
     typeSpeed: 5,
     smartBackspace: true,
-    shuffle: true,
+    shuffle: false,
     loop: false,
-    strings: [],
+    strings: ["Find our more about what I do"],
     startDelay: 0,
     onReset: function (self) {
         self.strings[0] = currentString
@@ -33,6 +33,8 @@ let options = {
 
 let typed = false;
 
+//typed = new Typed('#expand', options);
+
 
 
 $('.expertise .item').click(
@@ -42,6 +44,7 @@ $('.expertise .item').click(
 
             $('.linkbox.active').removeClass("active");
             $(this).find(".linkbox").addClass("active");
+            $('.expertise-expansion').slideDown();
 
 
             if (!typed) {
