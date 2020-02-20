8cdfee08e24d6cb46549a753c37fd4a36f1cb424
Joe Bailey-Roberts <dev@joebr.io>
2020-02-20 10:36:24 +0000

    Add spacing to banner text

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -25,7 +25,7 @@ $title = get_the_title( $id );
 <div class="container-fluid">
 <h1 style="background-image: url(<?php echo wp_get_attachment_image_url( $image_id, 'full' ); ?>)" data-title="<?php echo $title; ?>"><span><?php echo $title; ?><span></h1>
 </div>
-<div class="banner-content">
+<div class="banner-content container-fluid">
 <?php the_content() ?>
 </div>
 
