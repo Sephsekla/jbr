d63d5cd1d53b7a74adf2c0b63795349ef31742d0
Joe Bailey-Roberts <dev@joebr.io>
2020-02-21 15:58:53 +0000

    Sort parallax

--- template-parts/404.php
+++ template-parts/404.php
@@ -1,7 +1,30 @@
 <span class="screen-reader-text">404: Page not Found</span>
 <div class="container">
 <div class="wrapper-404" id="parallax">
-    <?php echo str_repeat('<span></span>',70); ?>
+    <?php
+    
+    for($i = 0; $i < 70; $i++ ){
+        switch($i%5){
+            case 0:
+                echo '<span data-depth="-1"></span>';
+            break;
+            case 1:
+                echo '<span data-depth="-2"></span>';
+            break;
+            case 2:
+                echo '<span data-depth="1"></span>';
+            break;
+            case 3:
+                echo '<span data-depth="2"></span>';
+            break;
+            default:
+                echo '<span data-depth="3"></span>';
+        }
+
+    }
+    
+    
+    ?>
    
 </div>
 </div>
