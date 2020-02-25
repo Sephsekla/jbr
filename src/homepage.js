e9573f23b50ba0c8401ccfb19ee58936a84575d7
Joe Bailey-Roberts <dev@joebr.io>
2020-02-25 10:29:45 +0000

    Refactor home js

--- src/homepage.js
+++ src/homepage.js
@@ -1,55 +1,15 @@
-// Can also be included with a regular script tag
-import Typed from 'typed.js';
-import he from 'he';
+import homeType from './js/hometype.js';
+
+import {
+    initHeaderScroll,
+    popInStagger
+} from './js/scroll.js';
 
 $ = jQuery;
 
 
-const gitMessage = `git clone joebr.git
-\`Cloning into 'joebr'...\`
-\`remote: Enumerating objects: 148, done.\`
-\`remote: Counting objects: 100% (148/148), done.\`
-\`remote: Compressing objects: 100% (107/107), done.\`
-\`remote: Total 1652 (delta 55), reused 113 (delta 36), pack-reused 1504\`
-\`Receiving objects: 100% (1652/1652), 2.77 MiB | 4.39 MiB/s, done.\`
-\`Resolving deltas: 100% (920/920), done.\`
+homeType();
 
-\`Hover over the site to see what's going on!\``;
+initHeaderScroll();
 
-let options = {
-  strings: [gitMessage,gitMessage],
-  typeSpeed: 30,
-  smartBackspace: true,
-  shuffle: true,
-  loop: false,
-  onComplete: function(){
-      initialComplete = true;
-  }
-};
-
-let initialComplete = false;
-
-let typed = new Typed('#home-screen', options);
-
-$('section, div').mouseenter(
-    function(){
-        if(initialComplete){
-        options.strings = [he.encode($(this).html())];
-        options.typeSpeed = 5;
-
-        typed.destroy();
-
-        typed = new Typed('#home-screen', options);
-        }
-
-    }
-);
-
-
-import {
-    initHeaderScroll, popInStagger
-  } from './js/scroll.js';
-  
-  initHeaderScroll();
-
-  //popInStagger('.link-wrapper','#links',200,0.8);
+//popInStagger('.link-wrapper','#links',200,0.8);
