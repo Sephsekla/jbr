78626f5843fcb0d809d88b2556806ddcc921e112
Joe Bailey-Roberts <dev@joebr.io>
2020-02-25 10:54:09 +0000

    Refactor js

--- src/wid.js
+++ src/wid.js
@@ -1,75 +1,9 @@
-// Can also be included with a regular script tag
-import Typed from 'typed.js';
-import he from 'he';
-import { popInStagger } from './js/scroll';
-
-/*const typestrings = [
-    '<section>Hola</section>',
-    '<section>Iawn</section>'
-    
-];
-
-const stringMap = typestrings.map( x => he.encode(x));
-*/
-
-$ = jQuery;
-
-let currentString;
+import {
+    popInStagger
+} from './js/scroll';
+import expertiseHover from './js/expertise';
 
 
-let options = {
+expertiseHover();
 
-    typeSpeed: 1,
-    smartBackspace: true,
-    shuffle: false,
-    loop: false,
-    strings: ["Find our more about what I do"],
-    startDelay: 0,
-    onReset: function (self) {
-        self.strings[0] = currentString
-    },
-
-
-};
-
-let typed = false;
-
-//typed = new Typed('#expand', options);
-
-
-
-$('.expertise .item').mouseenter(
-    function () {
-
-        if (!$(this).find(".linkbox").hasClass("active")) {
-
-            $('.linkbox.active').removeClass("active");
-            $(this).find(".linkbox").addClass("active");
-            $('.expertise-expansion').slideDown();
-
-            if(true){
-                $('#expand').text($(this).find('.slideout').text());
-            }
-            else if (!typed) {
-                options.strings[0] = $(this).find('.slideout').text();
-                typed = new Typed('#expand', options);
-                console.log('initial');
-            } else {
-
-                currentString = $(this).find('.slideout').text();
-
-                typed.reset(true);
-            }
-
-
-
-        }
-
-
-
-    }
-
-
-);
-
-popInStagger('.tech-item','#technologies',300);
+popInStagger('.tech-item', '#technologies', 300);
