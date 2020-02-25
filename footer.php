bed243366ca88923059cf30bbadb7ee7cb704522
Joe Bailey-Roberts <dev@joebr.io>
2020-02-25 10:24:21 +0000

    Split columns on footer

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
 <footer class="site-footer">
     <div class="container">
         <div class="row">
-            <div class="col-6"><p>&copy; Joe Bailey-Roberts <?php echo date('Y') ?></p></div>
-            <div class="col-6"> <?php
+            <div class="col-12 col-sm-6"><p>&copy; Joe Bailey-Roberts <?php echo date('Y') ?></p></div>
+            <div class="col-12 col-sm-6"> <?php
 					wp_nav_menu(
 						array(
 							'theme_location' => 'footer',
