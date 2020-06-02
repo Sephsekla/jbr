28f28857f368c5d6de8aac622f567b4df8afc777
Joe Bailey-Roberts <dev@joebr.io>
2020-06-02 18:25:51 +0100

    Fix repeated prism error

--- src/js/codehighlight.js
+++ src/js/codehighlight.js
@@ -9,10 +9,10 @@ require('submodules/prism-treeview/prism-treeview.js');
  *
  */
 function initHighlight() {
-    Prism.highlightAll(); // Hightlight all code with prismjs
+   // Prism.highlightAll(); // Hightlight all code with prismjs
 
     setTimeout(() => { // Just in case something loads in after page load
-        Prism.highlightAll();
+      //  Prism.highlightAll();
     }, 0);
 }
 
