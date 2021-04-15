ca5ee844f24ae58839f7eff59c5196b1e0e3765c
Joe Bailey-Roberts <dev@joebr.io>
2021-04-15 17:32:04 +0100

    Recreate functionality using gsap scrolltrigger

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,50 +1,58 @@
-
-//import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
-//import "imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js";
-import {gsap} from 'gsap';
-//import ScrollMagic from 'scrollmagic';
-//import "imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
+import {
+  gsap
+} from 'gsap';
 
 $ = jQuery;
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+  ScrollTrigger
+} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 
-function initHeaderScroll(){
+function initHeaderScroll() {
 
-	ScrollTrigger.create({
-		trigger: 'body',
-		start: 150,
-		//markers: true,
-		toggleClass: { targets: '#header-main', className: 'scrolled' },
-	  });
+  ScrollTrigger.create({
+    trigger: 'body',
+    start: 150,
+    //markers: true,
+    toggleClass: {
+      targets: '#header-main',
+      className: 'scrolled'
+    },
+  });
 
-                    
-}
-
-function popInStagger(items,trigger, duration=0, triggerPos='50%'){
-
-	const techIn = new gsap.timeline({
-		scrollTrigger: {
-			trigger: trigger,
-			once: true,
-			start: `top ${triggerPos}`,
-			markers: true,
-			end: 300,
-
-		}
-	},0.1);
-
-techIn.from(items,{
-		autoAlpha: 0,
-		scale: 2,
-		duration: duration,
-		stagger: 0.2,
-	},0.1);
 
 }
 
 
+function popInStagger(items, trigger, duration = 0, triggerPos = '50%') {
 
-export { initHeaderScroll, popInStagger};
+  new gsap.timeline({
+
+      scrollTrigger: {
+        trigger: trigger,
+        once: true,
+        start: `top ${triggerPos}`,
+        //markers: true,
+        end: '80% 70%',
+        scrub: 1,
+
+      }
+    }, 5)
+
+    .from(items, {
+      autoAlpha: 0,
+      scale: 2,
+      stagger: 0.2,
+      duration: duration,
+    }, 0.001);
+
+}
+
+
+
+export {
+  initHeaderScroll,
+  popInStagger
+};
