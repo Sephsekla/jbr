faebb39510812734373dcb8fef55a21e5b20d51a
Joe Bailey-Roberts <dev@joebr.io>
2021-04-16 09:49:34 +0100

    Update production build files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,32 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+//const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const WebpackShellPlugin = require('webpack-shell-plugin');
+//const WebpackShellPlugin = require('webpack-shell-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
 
 
  module.exports = merge(common, {
    mode: 'production',
-   plugins: [
+ /*  plugins: [
     new WebpackShellPlugin({
 
     })
-  ],
+  ], */
      optimization: {
+      minimize: true,
         minimizer: [
-          new UglifyJsPlugin({
+         /* new UglifyJsPlugin({
             cache: true,
             parallel: true,
             sourceMap: false // set to true if you want JS source maps
-          }),
-          new OptimizeCSSAssetsPlugin({})
+          }), */
+          new OptimizeCSSAssetsPlugin({}),
+          new TerserPlugin({
+            test: /\.js(\?.*)?$/i,
+            extractComments: true,
+          })
         ]
       },
  });
