4737a922b24f777a07b96a1510ccba141d2687d4
Joe Bailey-Roberts <dev@joebr.io>
2021-04-16 11:36:55 +0100

    Fix scripts error and update deps

--- inc/scripts.php
+++ inc/scripts.php
@@ -83,7 +83,7 @@ add_action(
 add_action( 'wp_print_styles', __NAMESPACE__ . '\\deregister', 100 );
 
 function deregister() {
-	if ( true || !is_user_logged_in() ) {
+	if ( !is_user_logged_in() ) {
 		wp_deregister_style( 'dashicons' );
 		wp_deregister_style( 'admin-bar' );
 	}
